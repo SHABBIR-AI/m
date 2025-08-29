c15b3cfb6ff429db4de9f18f9cacaf1268836f65
SHABBIR SHAIK <shabbirshaik1536@gmail.com>
2025-08-29 12:14:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: add item to cart
+function addToCart(product) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(product.name + " added to cart!");
+}
